58f80c09a9efc1f5f896039399d51e0464c3d401
Oleh Ohnivchuk <oohnivch@c3r3p1.42vienna.com>
2024-10-08 10:14:12 +0200

    green too dark

--- lua/neon/colors/cherrykiss-night.lua
+++ lua/neon/colors/cherrykiss-night.lua
@@ -3,5 +3,5 @@ local ret = vim.deepcopy(require("neon.colors.cherrykiss-storm"))
 ---@type Palette
 return vim.tbl_deep_extend("force", ret, {
   bg = "#1a1b26",
-  bg_dark = "#16161e",
+  bg_dark = "#000000",
 })
